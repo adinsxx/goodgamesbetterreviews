86bda976a77f855d94d1cf4d1912bc8a22509228
Brennan Reynolds <brennanj@proton.me>
2023-01-27 20:26:35 -0600

    Changes to be committed: 	deleted:    .eslintignore 	deleted:    .eslintrc.cjs 	deleted:    .gitignore 	deleted:    .npmrc 	deleted:    .prettierignore 	deleted:    .prettierrc 	deleted:    README.md 	new file:   client/.gitignore 	new file:   client/README.md 	new file:   client/package-lock.json 	new file:   client/package.json 	new file:   client/public/favicon.png 	new file:   client/public/global.css 	new file:   client/public/index.html 	new file:   client/rollup.config.js 	new file:   client/scripts/setupTypeScript.js 	new file:   client/src/App.svelte 	new file:   client/src/main.js 	deleted:    jsconfig.json 	deleted:    package.json 	deleted:    playwright.config.js 	new file:   server.py 	deleted:    src/app.d.ts 	deleted:    src/app.html 	deleted:    src/index.test.js 	deleted:    src/routes/+page.svelte 	deleted:    static/favicon.png 	deleted:    svelte.config.js 	deleted:    tests/test.js 	deleted:    vite.config.js

---
+++ client/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	let rand = -1;
+	function getRand() {
+	  fetch("./rand")
+		.then(c => c.text())
+		.then(c => (rand = c));
+	}
+  </script>
+  
+  <h1>Your number is {rand}!</h1>
+  <button on:click={getRand}>Get a random number</button>
